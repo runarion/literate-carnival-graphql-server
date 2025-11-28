959675397dfaa7c9e54bfd5400c21b64f6107ded
runarion <runarion@cyber-master.com>
2025-11-28 16:41:55 +0100

    feat: update resolvers with queries and mutations

--- graphql/data/resolvers.js
+++ graphql/data/resolvers.js
@@ -1,33 +1,63 @@
 import { Widgets } from "./dbConnectors.js";
 
 const resolvers = {
-  getProduct: async ({id}) => {
-    try {
-      const product = await Widgets.findById(id);
-      return product;
-    } catch (error) {
-      throw new Error("Error fetching product: " + error.message);
-    }
-  },
+    getProduct: async ({id}) => {
+        try {
+            const product = await Widgets.findById(id);
+            return product;
+        } catch (error) {
+            throw new Error("Error fetching product: " + error.message);
+        }
+    },
 
-  getAllProducts: async () => {
-    try {
-      const products = await Widgets.find({});
-      return products;
-    } catch (error) {
-      throw new Error("Error fetching products: " + error.message);
-    }
-  },
+    getAllProducts: async () => {
+        try {
+            const products = await Widgets.find({});
+            return products;
+        } catch (error) {
+            throw new Error("Error fetching products: " + error.message);
+        }
+    },
 
-  createProduct: async ({input}) => {
-    try {
-      const newProduct = new Widgets(input);
-      await newProduct.save();
-      return newProduct;
-    } catch (error) {
-      throw new Error("Error creating product: " + error.message);
+    createProduct: async ({input}) => {
+        const newWidget = new Widgets({
+            name: input.name,
+            description: input.description,
+            price: input.price,
+            soldOut: input.soldOut,
+            inventory: input.inventory,
+            stores: input.stores,
+        });
+
+        newWidget.id = newWidget._id;
+    
+        try {
+            await newWidget.save();
+            return newWidget;
+        } catch (error) {
+            throw new Error("Error creating product: " + error.message);
+        }
+    },
+
+    updateProduct: async ({input}) => {
+        try {
+            const updatedWidget = await Widgets.findOneAndUpdate(
+                {_id: input.id}, input, {new: true}
+            );
+            return updatedWidget;
+        } catch (error) {
+            throw new Error("Error updating product: " + error.message);
+        }
+    },
+
+    deleteProduct: async ({id}) => {
+        try {
+            await Widgets.deleteOne({_id: id});
+            return "Successfully deleted product with id: " + id;
+        } catch (error) {
+            throw new Error("Error deleting product: " + error.message);
+        }
     }
-  }   
-}
+};
 
 export default resolvers;
--- graphql/data/schema.js
+++ graphql/data/schema.js
@@ -41,6 +41,8 @@ const schema = buildSchema(`
 
     type Mutation {
         createProduct(input: ProductInput): Product
+        updateProduct(input: ProductInput): Product
+        deleteProduct(id: ID!): String
     }
 
 `);
